6696ba53be1be9f3d7b689d0020184bc0b244b6e
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2023-02-20 14:55:16 +0200

    Removed author from metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,11 +3,6 @@ module.exports = {
 	"url": "https://webtoknow.com/",
 	"language": "en",
 	"description": "Explore web technologies, IOT, security, and IT jobs with our informative blog featuring in-depth tutorials and articles.",
-	"author": {
-		"name": "Bogdan Nicolae",
-		"email": "bmnicolae@gmail.com",
-		"url": "https://bogminic.com"
-	},
 	"authors": [
     {
       "key":"bogminic",
